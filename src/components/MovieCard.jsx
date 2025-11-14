fb59b656869bd09765dca5d4531a64f76065bc5a
Amira Elsayed Abdelaziz <amira.elsayed.abdelaziz@gmail.com>
2025-11-15 00:58:37 +0200

    using imdb API instead of TMDB

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,29 +1,35 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faPlus, faStar } from "@fortawesome/free-solid-svg-icons"
 import { Link } from "react-router-dom"
+import React from "react"
+export default function MovieCard({ id }) {
+    const [movie, setMovie] = React.useState({})
+    React.useEffect(() => {
+        fetch(`http://www.omdbapi.com/?apikey=bbc3879d&i=${id}`)
+            .then(res => res.json())
+            .then(data => setMovie(data))
 
-export default function MovieCard({movie}) {
-    console.log(movie)
+    }, [])
     return (
         <div className="movie-card">
             <div className="card-poster">
-                <img className="poster" src={movie.poster_path} />
+                <img className="poster" src={movie.Poster} />
 
             </div>
             <div className="card-details">
                 <div>
-                    <h4>{movie.title}</h4>
+                    <h4>{movie.Title}</h4>
                     <span className="rate">
-                        <FontAwesomeIcon icon={faStar} />{movie.vote_average}
-                        </span>
+                        <FontAwesomeIcon icon={faStar} />{movie.imdbRating}
+                    </span>
 
                 </div>
                 <div>
-                    {/* <span className="type">{movie.Genre}</span> */}
-                    <span className="duration">{movie.release_date}</span>
+                    <span className="type">{movie.Genre}</span>
+                    <span className="duration">{movie.Runtime}</span>
 
                 </div>
-                <div className="description">{movie.overview}</div>
+                <div className="description">{movie.Plot}</div>
                 <Link className="add"><FontAwesomeIcon icon={faPlus} />Watch List</Link>
 
             </div>
--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard"
 import React from "react"
 import axios from "axios"
 export default function SearchResult({ search }) {
-    const [allMovies, setAllMovies] = React.useState([])
+    const [allMoviesId, setAllMoviesId] = React.useState([])
     const options = {
         method: "GET",
         headers: {
@@ -15,8 +15,11 @@ export default function SearchResult({ search }) {
     };
 
     React.useEffect(() => {
-        axios.get(`https://api.themoviedb.org/3/search/movie?query=${search}&page=1`, options)
-            .then(data => setAllMovies(data.data.results))
+        // axios.get(`https://api.themoviedb.org/3/search/movie?query=${search}&page=1`, options)
+        //     .then(data => setAllMovies(data.data.results))
+        fetch(`http://www.omdbapi.com/?apikey=bbc3879d&s=${search}`)
+            .then(res =>res.json())
+            .then(data => setAllMoviesId(data.Search.map(ele => ele.imdbID)))
     }, [search])
     return (
         <section className="result">
@@ -29,8 +32,8 @@ export default function SearchResult({ search }) {
                         <h2>Start exploring</h2> </>
                 )
                 :
-                allMovies ? (
-                    allMovies.map(ele=><MovieCard movie={{...ele, poster_path:`https://image.tmdb.org/t/p/w500${ele.poster_path}`}} />)
+                allMoviesId ? (
+                    allMoviesId.map(ele => <MovieCard key={ele} id={ele} />)
                 ) : (<h1>Loading</h1>)
             }
         </section>
